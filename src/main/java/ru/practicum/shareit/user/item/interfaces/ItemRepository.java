838af2743fb6ae40dec6fbe191baba8c1cb37989
Dmitry <dm1try-teter1n@yandex.ru>
2023-05-24 20:52:04 +0500

    after review-2

--- src/main/java/ru/practicum/shareit/user/item/interfaces/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/user/item/interfaces/ItemRepository.java
@@ -3,10 +3,12 @@ package ru.practicum.shareit.user.item.interfaces;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.item.model.Item;
 
 import java.util.Collection;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     Collection<Item> getItemsByOwnerId(Long ownerId);
